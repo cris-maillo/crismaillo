467966c9a9c7618b3f6c01584918e71cb50b7bef
Cris <cristinamaillo.r@gmail.com>
2023-10-28 23:45:27 +0100

    added images header and background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './../index.css';
-import yellow from '../assets/images/yellow2.svg';
+import yellow2 from '../assets/images/yellow2.svg';
+import yellow from '../assets/images/yellow.svg';
+import green from '../assets/images/green.svg';
 
 
 function Background() {
   return (
     <div className="background">
         <div>
-            <img className="greenSvg" src="./imgs/green.svg" alt="Green" />
+            <img className="greenSvg" src={green} alt="Green" />
         </div>
         <div>
-            <img className="yellowSvg" src="./imgs/yellow.svg" alt="Yellow" style={{ width: "200px", top: "-120px", left: "80px" }} />
+            <img className="yellowSvg" src={yellow} alt="Yellow" style={{ width: "200px", top: "-120px", left: "80px" }} />
         </div>
         <div>
-            <img className="yellowSvg bottom" src={yellow} alt="Yellow 2" />
+            <img className="yellowSvg bottom" src={yellow2} alt="Yellow 2" />
         </div>
     </div>
 )}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import './../index.css';
+import ellipse from '../assets/images/ellipse.svg';
+import smiley from '../assets/images/smiley.svg';
 
 function Header() {
   return (
-
-
-      <div className="container">
-        <div className="logo">
-          <h1><a href="index.html">Cris Maillo</a></h1>
-          <img className="smileySvg" src="../assets/images/smiley.svg" alt="Smiley" />
-          <div className="ellipseContainer">
-            <a href="index.html">
-              <img className="ellipseSvg" src="../assets/images/ellipse.svg" alt="Ellipse" />
-              <img className="ellipseSvg" src="../assets/images/ellipse.svg" alt="Ellipse" style={{ transform: "rotate(10deg)" }} />
-            </a>
-          </div>
+    <div className="container">
+      <div className="logo">
+        <h1><a href="index.html">Cris Maillo</a></h1>
+        <img className="smileySvg" src={smiley} alt="Smiley" />
+        <div className="ellipseContainer">
+          <a href="index.html">
+            <img className="ellipseSvg" src={ellipse} alt="Ellipse" />
+            <img className="ellipseSvg" src={ellipse} alt="Ellipse" style={{ transform: "rotate(10deg)" }} />
+          </a>
         </div>
+      </div>
 
-        <div className="divider">
-          <hr />
-        </div>
+      <div className="divider">
+        <hr />
+      </div>
     </div>
-
   );
 }
 
